90fd2b85326aca39f55972923e4d8d30f0cae3dd
KMacdonald <kylemacdo01@gmail.com>
2024-07-19 10:22:37 -0400

    Added pygame materials

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import pygame
+print(pygame.ver)
+pygame.init
+screen = pygame.display.set_mode((1920, 1080))
+clock = pygame.time.Clock
+running = True
+mousePosition = pygame.mouse.get_pos
+
+while running:
+    screen.fill("black")
+    mouseX, mouseY = pygame.mouse.get_pos()
+    pygame.draw.circle(screen, "red", (mouseX, mouseY), 30)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+pygame.quit()
+
 roaddata = [[]]
 trafficTypes = ["Road", "Stoplight", "Pedestrian Crossing", "Stop Sign"]
 import roads
