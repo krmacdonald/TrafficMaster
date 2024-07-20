9457cace9b2d3446e9d8b5f1614dbeba4381ca75
KMacdonald <kylemacdo01@gmail.com>
2024-07-20 11:41:26 -0400

    Created Mouse Button Pressed logic

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ resolutionx, resolutiony = monitors[0]
 screen = pygame.display.set_mode((resolutionx, resolutiony))
 clock = pygame.time.Clock
 running = True
-mousePosition = pygame.mouse.get_pos
+mousePosition = pygame.mouse.get_pos()
 pygame.mouse.set_visible(False)
 mouseSprite = pygame.image.load("Resources/Images/mouse.png")
 imagerect = mouseSprite.get_rect()
@@ -27,8 +27,12 @@ while running:
     
     if pygame.key.get_focused():
         keys = pygame.key.get_pressed()
+        mouse = pygame.mouse.get_pressed(num_buttons=3)
         if keys[pygame.K_ESCAPE]:
             running = False
+        if mouse[0]:
+            pygame.draw.rect(screen, "red", pygame.rect.Rect(100, 100, 300, 300))
+        
     
     pygame.display.flip()
 
